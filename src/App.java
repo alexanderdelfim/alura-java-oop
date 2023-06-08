d57cb7c121ff33008c2f61573bc2c29a8447b92b
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2023-06-07 21:38:57 -0300

    assigning values to our film

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Movie myMovie = new Movie();
+        myMovie.name = "O poderoso chefão";
+        myMovie.releaseYear = 1970;
+        myMovie.durationInMinutes = 180;
+
+        System.out.println(myMovie.name);
+        System.out.println(myMovie.releaseYear);
     }
 }
